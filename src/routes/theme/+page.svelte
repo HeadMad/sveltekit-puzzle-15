0e59e4212f17b7f3829069b4439a6ef0243cb525
headmad <kov.ps@ya.ru>
2024-11-25 16:54:46 +0800

    add theme section

---
+++ src/routes/theme/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let TWA;
+  $effect(() => {
+    TWA = window.Telegram.WebApp;
+
+  })
+</script>
+<svelte:head>
+  <script src="https://telegram.org/js/telegram-web-app.js?56"></script>
+</svelte:head>
+
+<pre>
+  {JSON.stringify(TWA?.themeParams, null, 2)}
+</pre>
